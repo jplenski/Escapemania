e26a5718626bb4a627b52abb8529ac1fa92fc8c2
Joseph Lenski <jplenski12@gmail.com>
2023-11-09 17:53:29 -0500

    Player Camera Movement

---
+++ Escapemania/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+  public float sensitivity = 500f;
+  public Transform camera;
+  public float xRotation = 0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Get mouse input
+    float x = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+    float y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+    xRotation -= y;
+    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    camera.Rotate(Vector3.up * x);
+  }
+}
