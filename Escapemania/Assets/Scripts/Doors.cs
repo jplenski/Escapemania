b7ff64ab438ea1831135a17a33ffe41e7df76905
Joseph Lenski <jplenski12@gmail.com>
2023-11-29 19:08:13 -0500

    Win Screen for Demo

--- Escapemania/Assets/Scripts/Doors.cs
+++ Escapemania/Assets/Scripts/Doors.cs
@@ -7,10 +7,12 @@ public class Doors : MonoBehaviour
   public List<IsTouching> doors;
   public GameObject door1;
   public GameObject key1;
+  public GameObject winScreen;
   // Start is called before the first frame update
   void Start()
   {
     door1.SetActive(true);
+    winScreen.SetActive(false);
   }
 
   // Update is called once per frame
@@ -20,6 +22,9 @@ public class Doors : MonoBehaviour
     {
       door1.SetActive(false);
       key1.SetActive(false);
+      winScreen.SetActive(true);
+      Time.timeScale = 0f;
+      Cursor.lockState = CursorLockMode.None;
     }
   }
 }
